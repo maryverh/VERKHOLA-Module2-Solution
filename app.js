2dab67d57308ea5dfa395836b5451f7a28d18882
maryverh <m_verhola@ukr.net>
2016-09-27 15:05:35 +0300

    Assignment_2

--- app.js
+++ app.js
@@ -1,35 +1,87 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchController', LunchController);
+var items=[
+  {
+    name: "Milk",
+    quantity: "2"
+  },
+  {
+    name: "Donuts",
+    quantity: "200"
+  },
+  {
+    name: "Cookies",
+    quantity: "300"
+  },
+  {
+    name: "Chocolate",
+    quantity: "5"
+  },
+  {
+    name: "Bread",
+    quantity: "2"
+  },
+  {
+    name: "Cheese",
+    quantity: "1"
+  }
+];
 
-LunchController.$inject = ['$scope'];
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
-function LunchController($scope) {
-  $scope.menues="";
-  $scope.message="";	
-  $scope.ctrlLunch = function () {
-    if($scope.menues){
-    	var sep=",";
-    	var menuItems=$scope.menues.split(sep);
-    	var counter=0;
-    	for (var i=0; i<menuItems.length; i++){
-    		if(!(menuItems[i].length==0||!menuItems[i].trim())){
-    			counter+=1;
-    		}
-    	}
-    	if(counter>3){
-    		$scope.message="Too much!";
-    	}else{
-    		$scope.message="Enjoy!";
-    	}
-    }
-    else{
-    	$scope.message="Please enter data first";
-    }
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var itemList = this;
+
+  itemList.items = ShoppingListCheckOffService.getItems();
+  itemList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };  
+
+  itemList.empty = function () {
+    return itemList.items.length === 0;
   };
- };   
+}
 
-    
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+
+  boughtList.items = ShoppingListCheckOffService.boughtItems();
+
+  boughtList.empty = function () {
+    return boughtList.items.length === 0;
+  };
+  
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var tobuy_items = items
+  var bought_items=[] ;
+  
+  // List of shopping items
+  
+
+  service.removeItem = function (itemsIndex) {
+    bought_items.push(tobuy_items[itemsIndex]);
+    tobuy_items.splice(itemsIndex, 1);
+  };
+
+ 
+  service.getItems = function () {
+    return tobuy_items;
+  };
+
+   service.boughtItems = function () {
+    return bought_items;
+  };
+}
+
 })();
